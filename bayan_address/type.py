63d09d240fc10f784cc3fc240de6168a0ed8ffe0
Netervati <netervati@gmail.com>
2022-10-19 22:30:13 +0800

    Merge v2

--- bayan_address/type.py
+++ bayan_address/type.py
@@ -1,20 +1,23 @@
 from typing import Union
-from .lib.data import ADDRESS_FORMAT, PROVINCES
-from .lib.utils import clean_str
+from .lib.data import ADDRESS_FORMAT, CITIES, PROVINCES
+from .lib.utils import clean_str, replace_str
 
 
 def get_address_type(item: str) -> dict:
     stripped_item = item.strip()
+    if is_building_no(stripped_item):
+        return {"building": stripped_item}
     if is_valid_zipcode(stripped_item):
         return {"zip_code": stripped_item}
 
     cleaned_address = clean_str(item)
     for k, v in ADDRESS_FORMAT.items():
         for el in v:
-            if clean_str(el) in cleaned_address:
+            cleaned_element = clean_str(el)
+            if cleaned_element in cleaned_address:
                 return {k: stripped_item}
 
-    return {"barangay": stripped_item}
+    return {"undefined": stripped_item}
 
 
 def get_province_related_type(val: str) -> Union[str, None]:
@@ -24,8 +27,50 @@ def get_province_related_type(val: str) -> Union[str, None]:
             return PROVINCES[el]
 
 
+def is_building_no(val: str) -> bool:
+    if len(val) == 4 or not val.isdigit():
+        return False
+
+    return True
+
+
 def is_valid_zipcode(val: str) -> bool:
     if len(val) != 4 or not val.isdigit():
         return False
 
     return True
+
+
+def strip_matching_data(val: str) -> dict:
+    pre_selected_formats = {}
+    stripped_address = val
+
+    cleaned_str = clean_str(val)
+    if "metro manila" in cleaned_str:
+        stripped_address = replace_str("metro manila", stripped_address)
+        pre_selected_formats["administrative_region"] = "Metro Manila"
+
+    for el in PROVINCES:
+        if clean_str(el) in cleaned_str:
+            stripped_address = replace_str(el, stripped_address)
+            pre_selected_formats["province"] = el
+            break
+
+    cleaned_str = clean_str(stripped_address)
+    for el in CITIES:
+        cleaned_element = clean_str(el)
+        if cleaned_element in cleaned_str:
+            stripped_address = replace_str(el, stripped_address)
+            pre_selected_formats["city"] = el
+            break
+        elif "city" in cleaned_element:
+            cleaned_element = replace_str("city", cleaned_element).strip()
+            if cleaned_element in cleaned_str:
+                stripped_address = replace_str(cleaned_element, stripped_address)
+                pre_selected_formats["city"] = cleaned_element.capitalize()
+                break
+
+    return {
+        "pre_selected_formats": pre_selected_formats,
+        "stripped_address": stripped_address,
+    }
--- tests/type_tests.py
+++ tests/type_tests.py
@@ -2,7 +2,9 @@ import pytest
 from bayan_address.type import (
     get_address_type,
     get_province_related_type,
+    is_building_no,
     is_valid_zipcode,
+    strip_matching_data,
 )
 
 
@@ -15,8 +17,18 @@ def mock_cleanstr(mocker):
 
 
 @pytest.fixture
+def mock_isbuildingno(mocker):
+    return mocker.patch(f"{TYPE_PATH}is_building_no")
+
+
+@pytest.fixture
 def mock_isvalidzipcode(mocker):
     return mocker.patch(f"{TYPE_PATH}is_valid_zipcode")
+
+
+@pytest.fixture
+def mock_replacestr(mocker):
+    return mocker.patch(f"{TYPE_PATH}replace_str")
 
 
 @pytest.fixture
@@ -25,16 +37,30 @@ def monkeypatch_address_format(monkeypatch):
 
 
 @pytest.fixture
+def monkeypatch_cities(monkeypatch, cities_fixture):
+    return monkeypatch.setattr(f"{TYPE_PATH}CITIES", cities_fixture)
+
+
+@pytest.fixture
 def monkeypatch_provinces(monkeypatch, provinces_fixture):
     return monkeypatch.setattr(f"{TYPE_PATH}PROVINCES", provinces_fixture)
 
 
 @pytest.mark.parametrize(
-    ["arg", "isvalidzipcode", "addressformat", "cleanstr_sideeffect", "result"],
     [
-        ("1000", True, [], [], {"zip_code": "1000"}),
+        "arg",
+        "isbuildingno",
+        "isvalidzipcode",
+        "addressformat",
+        "cleanstr_sideeffect",
+        "result",
+    ],
+    [
+        ("24", True, False, [], [], {"building": "24"}),
+        ("1000", False, True, [], [], {"zip_code": "1000"}),
         (
             "Test City",
+            False,
             False,
             {"city": ["city"]},
             ["test city", "city"],
@@ -43,9 +69,10 @@ def monkeypatch_provinces(monkeypatch, provinces_fixture):
         (
             "Fallback",
             False,
+            False,
             {"city": ["city"]},
             ["fallback", "city"],
-            {"barangay": "Fallback"},
+            {"undefined": "Fallback"},
         ),
     ],
 )
@@ -53,18 +80,22 @@ def test_get_address_type(
     addressformat,
     arg,
     cleanstr_sideeffect,
+    isbuildingno,
     isvalidzipcode,
     mock_cleanstr,
+    mock_isbuildingno,
     mock_isvalidzipcode,
     monkeypatch,
     result,
 ):
+    mock_isbuildingno.return_value = isbuildingno
     mock_isvalidzipcode.return_value = isvalidzipcode
     monkeypatch.setattr(f"{TYPE_PATH}ADDRESS_FORMAT", addressformat)
     mock_cleanstr.side_effect = cleanstr_sideeffect
 
     assert get_address_type(arg) == result
-    mock_isvalidzipcode.assert_called_with(arg)
+    if isbuildingno is False:
+        mock_isvalidzipcode.assert_called_with(arg)
     if isvalidzipcode is False:
         mock_cleanstr.call_count == 2
 
@@ -94,9 +125,71 @@ def test_get_province_related_type(
     ["arg", "result"],
     [
         ("", False),
+        ("10", True),
+        ("1000", False),
+    ],
+)
+def test_is_building_no(arg, result):
+    assert is_building_no(arg) == result
+
+
+@pytest.mark.parametrize(
+    ["arg", "result"],
+    [
+        ("", False),
         ("10", False),
         ("1000", True),
     ],
 )
 def test_is_valid_zipcode(arg, result):
     assert is_valid_zipcode(arg) == result
+
+
+@pytest.mark.parametrize(
+    ["arg", "cleanstr_sideeffect", "replacestr_sideeffect", "result"],
+    [
+        (
+            "Metro Manila",
+            ["metro manila", "province", "", "bayan city"],
+            ["", "bayan", ""],
+            {
+                "pre_selected_formats": {"administrative_region": "Metro Manila"},
+                "stripped_address": "",
+            },
+        ),
+        (
+            "Province",
+            ["province", "province", "", "bayan city"],
+            ["", "bayan", ""],
+            {"pre_selected_formats": {"province": "Province"}, "stripped_address": ""},
+        ),
+        (
+            "Bayan City",
+            ["bayan city", "province", "bayan city", "bayan city"],
+            ["", "bayan", ""],
+            {"pre_selected_formats": {"city": "Bayan City"}, "stripped_address": ""},
+        ),
+        (
+            "Bayan",
+            ["bayan", "province", "bayan", "bayan city"],
+            ["bayan", "", ""],
+            {"pre_selected_formats": {"city": "Bayan"}, "stripped_address": ""},
+        ),
+    ],
+)
+def test_strip_matching_data(
+    arg,
+    cleanstr_sideeffect,
+    mock_cleanstr,
+    mock_replacestr,
+    monkeypatch_cities,
+    monkeypatch_provinces,
+    replacestr_sideeffect,
+    result,
+):
+    mock_cleanstr.side_effect = cleanstr_sideeffect
+    mock_replacestr.side_effect = replacestr_sideeffect
+
+    subject = strip_matching_data(arg)
+
+    assert subject == result
