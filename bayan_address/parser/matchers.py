18d8744d68bcf9bd26153b2ca4135eb9671b1ece
Netervati <netervati@gmail.com>
2022-10-26 01:30:14 +0800

    Ref#1 - Create matcher function for each address type

--- bayan_address/parser/matchers.py
+++ bayan_address/parser/matchers.py
@@ -1,3 +1,4 @@
+from typing import Union
 from bayan_address.lib.data import ADDRESS_PREFIX, CITIES, PROVINCES, STREET_FORMAT
 from bayan_address.lib.utils import (
     clean_str,
@@ -9,21 +10,43 @@ from bayan_address.lib.utils import (
 
 
 def match_address_type(val: str) -> dict:
-    patterns = {
-        "administrative_region": ["metro manila"],
-        "province": PROVINCES,
-        "zip_code": [r"\d{4}"],
-    }
-
     address = {}
-    stripped = replace_str(",", val).strip()
+    matchers = [
+        match_administrative_region,
+        match_province,
+        match_zip_code,
+        match_city,
+        match_street,
+        match_subdivision,
+        match_barangay,
+    ]
+    stripped = val
 
-    for k, v in patterns.items():
-        for el in v:
-            if res := match_pattern(el, stripped):
-                address[k] = res[0]
-                stripped = res[1]
-                break
+    for parse in matchers:
+        if result := parse(stripped):
+            stripped = result[0]
+            address |= result[1]
+
+    return address
+
+
+# Matchers based on the address type
+# ==================================
+
+
+def match_administrative_region(arg: str) -> Union[dict, None]:
+    if res := match_pattern("metro manila", arg):
+        return (res[1], {"administrative_region": res[0]})
+
+
+def match_barangay(arg: str) -> Union[dict, None]:
+    if is_valid_str(arg):
+        return ("", {"barangay": arg.strip()})
+
+
+def match_city(arg: str) -> Union[dict, None]:
+    address_city = None
+    stripped = arg
 
     for el in CITIES:
         cleaned_element = clean_str(el)
@@ -31,28 +54,44 @@ def match_address_type(val: str) -> dict:
         # Ensures that if city with no "City" in name will match
         # with address that has City (e.g. Quezon == Quezon City)
         if res := match_pattern(f"{el} city", stripped):
-            address["city"] = res[0]
+            address_city = res[0]
             stripped = res[1]
         elif res := match_pattern(el, stripped):
             if "city" in clean_str(res[1]):
                 if res_b := match_pattern(f"{el} city", stripped):
-                    address["city"] = res_b[0]
+                    address_city = res_b[0]
                     stripped = res_b[1]
                 else:
                     continue
             else:
-                address["city"] = res[0]
+                address_city = res[0]
                 stripped = res[1]
         # Ensures that if city with "City" in name will match
         # with address that has no City (e.g. Quezon City == Quezon)
         elif "city" in cleaned_element:
             cleaned_element = replace_str("city", cleaned_element).strip()
             if res := match_pattern(cleaned_element, stripped):
-                address["city"] = res[0]
+                address_city = res[0]
                 stripped = res[1]
 
-        if "city" in address:
+        if address_city:
             break
+
+    if address_city:
+        return (stripped, {"city": address_city})
+
+
+def match_province(arg: str) -> Union[dict, None]:
+    for el in PROVINCES:
+        if res := match_pattern(el, arg):
+            province_dict = {"province": res[0]} | PROVINCES[res[0]]
+            return (res[1], province_dict)
+
+
+def match_street(arg: str) -> Union[dict, None]:
+    address_building = None
+    address_street = None
+    stripped = arg
 
     prefixes = ADDRESS_PREFIX.split() + [""]
     for pref in prefixes:
@@ -64,24 +103,32 @@ def match_address_type(val: str) -> dict:
                 after=x,
             ):
                 if resb := match_pattern(r"\b\d+\b", res[0]):
-                    address["street"] = resb[1]
-                    address["building"] = resb[0]
+                    address_street = resb[1]
+                    address_building = resb[0]
                     stripped = res[1]
                     break
-                address["street"] = res[0]
+                address_street = res[0]
                 stripped = res[1]
                 break
 
-        if "street" in address:
+        if address_street:
             break
 
+    if address_street:
+        address_dict = {"street": address_street}
+        if address_building:
+            address_dict |= {"building": address_building}
+
+        return (stripped, address_dict)
+
+
+def match_subdivision(arg: str) -> Union[dict, None]:
     if res := match_in_between_pattern(
-        r"(.*?)subdivision+\b", stripped, before="", after="Subdivision"
+        r"(.*?)subdivision+\b", arg, before="", after="Subdivision"
     ):
-        address["subdivision"] = res[0]
-        stripped = res[1]
+        return (res[1], {"subdivision": res[0]})
 
-    if is_valid_str(stripped):
-        address["barangay"] = stripped.strip()
 
-    return address
+def match_zip_code(arg: str) -> Union[dict, None]:
+    if res := match_pattern(r"\d{4}", arg):
+        return (res[1], {"zip_code": res[0]})
