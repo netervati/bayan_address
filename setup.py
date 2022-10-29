8ef0f492509a3781e9cf7524785d8f7370f7b235
Netervati <netervati@gmail.com>
2022-10-30 02:50:47 +0800

    Ref#6 - Run black

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup
 
 setup(
-    name='bayan_address',
-    version='0.1.1',    
-    description='A Python-based address parser for Philippines',
-    url='https://github.com/netervati/bayan_address',
-    author='Christopher Tabula',
-    author_email='netervati@gmail.com',
-    license='MIT License',
-    packages=['bayan_address', 'bayan_address/parser', 'bayan_address/lib'],
-
+    name="bayan_address",
+    version="0.1.1",
+    description="A Python-based address parser for Philippines",
+    url="https://github.com/netervati/bayan_address",
+    author="Christopher Tabula",
+    author_email="netervati@gmail.com",
+    license="MIT License",
+    packages=["bayan_address", "bayan_address/parser", "bayan_address/lib"],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',  
-        'Operating System :: Microsoft :: Windows :: Windows 10',        
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Programming Language :: Python :: 3.9",
     ],
 )
