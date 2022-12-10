5859c479a2d6e5834fec598ab3b97fc4bdc09a47
netervati <netervati@gmail.com>
2022-12-10 20:07:54 +0800

    Ref#13 - Add new typings for match pattern functions

--- bayan_address/lib/_typings.py
+++ bayan_address/lib/_typings.py
@@ -1,4 +1,6 @@
-from typing import Union
+from typing import Union, NamedTuple
 
 
+MatchResult = NamedTuple("MatchResult", [("address_type", str), ("stripped", str)])
+MatchPattern = Union[MatchResult, None]
 ParsedAddressType = Union[tuple[str, dict], None]
